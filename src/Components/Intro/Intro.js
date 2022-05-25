ca1678e6e8986963bfe29b0afe3bd5913ed4b7eb
Thomas-Raveneau <thomas.raveneau1@gmail.com>
2022-05-25 14:51:31 +0900

    [ADD] - Intro with text anim and arrow down button anim

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
-import QuakeImage from '../../images/QuakeIntroImage.png';
+import QuakeIntroImage from '../../images/QuakeIntroImage.png';
 import SocialDungeonsIntroImage from '../../images/SocialDungeonsIntroImage.png';
 import ScratchyIntroImage from '../../images/ScratchyIntroImage.png'
+
+import ArrowDownImage from '../../images/arrow-down.svg'
 
 import './Intro.css'
 
 const Intro = () => {
+
+    const handleDownNavigation = () => {
+        console.log("here")
+    };
+
     return (
         <div className='container'>
             <div className='images-container'>
                 <div className='image-container'>
-                    <img className='image-intro' src={SocialDungeonsIntroImage} alt='tmp'></img>
+                    <img className='image-intro' src={SocialDungeonsIntroImage} alt='SocialDungeons game by Thomas Raveneau screenshot'></img>
                 </div>
                 <div className='image-container'>
-                    <img className='image-intro' src={QuakeImage} alt='tmp'></img>
+                    <img className='image-intro' src={QuakeIntroImage} alt='Quake (remake) game by Thomas Raveneau screenshot'></img>
                 </div>
                 <div className='image-container'>
-                    <img className='image-intro' src={ScratchyIntroImage} alt='tmp'></img>
+                    <img className='image-intro' src={ScratchyIntroImage} alt='Scratcy graphical library by Thomas Raveneau screenshot'></img>
                 </div>
             </div>
             <div className='texts-container'>
@@ -27,6 +34,11 @@ const Intro = () => {
                 <div className='text-container'>
                     <p className='text job'>Game programmer</p>
                 </div>
+            </div>
+            <div className='arrow-container'>
+                <button className='arrow-button' onClick={handleDownNavigation}>
+                    <img className='arrow-image' src={ArrowDownImage} alt='Double arrow down image to click on'></img>
+                </button>
             </div>
         </div>
     );
