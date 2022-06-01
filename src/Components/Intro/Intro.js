95f334b8da12f6539db97a6c360b083f54749494
Thomas-Raveneau <thomas.raveneau1@gmail.com>
2022-06-01 15:57:51 +0900

    [ADD] - intro responsive

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -21,30 +21,29 @@ const Intro = () => {
     }
 
     return (
-        <section className='intro-container'>
-            <div className='images-container'>
-                <div className='image-container'>
+        <section className='container-fluid bg-color p-0'>
+            <div className='row fill m-0'>
+                <div className='col-lg-4 col-md-12 col-sm-12 p-0 image-container'>
                     <img className='image-intro' src={SocialDungeonsIntroImage} alt='SocialDungeons game by Thomas Raveneau screenshot'></img>
                 </div>
-                <div className='image-container'>
+                <div className='col-lg-4 col-md-12 col-sm-12 p-0 image-container'>
                     <img className='image-intro' src={QuakeIntroImage} alt='Quake (remake) game by Thomas Raveneau screenshot'></img>
                 </div>
-                <div className='image-container'>
+                <div className='col-lg-4 col-md-12 col-sm-12 p-0 image-container'>
                     <img className='image-intro' src={ScratchyIntroImage} alt='Scratcy graphical library by Thomas Raveneau screenshot'></img>
                 </div>
             </div>
-            <div className='texts-container'>
-                <div className='text-container'>
-                    <h1 className='text name'>THOMAS RAVENEAU</h1>
+
+            <div className='over-container container-fluid fill position-absolute p-0'>
+                <div className='over-text-container'>
+                    <h1 className='over-text name-text'>THOMAS RAVENEAU</h1>
+                    <h2 className='over-text job-text'>Game programmer</h2>
                 </div>
-                <div className='text-container'>
-                    <h2 className='text job'>Game programmer</h2>
+                <div className='over-arrow-container'>
+                    <button className='arrow-button' onClick={scrollToPrologue}>
+                        <img className='arrow-image' src={ArrowDownImage} alt='Double arrow down button'></img>
+                    </button>
                 </div>
-            </div>
-            <div className='arrow-container'>
-                <button className='arrow-button' onClick={scrollToPrologue}>
-                    <img className='arrow-image' src={ArrowDownImage} alt='Double arrow down button'></img>
-                </button>
             </div>
         </section>
     );
